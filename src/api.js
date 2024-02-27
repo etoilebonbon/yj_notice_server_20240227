8debe3e1e761e2abb9f104d429a970e5646d917a
LDH <ai04120@naver.com>
2024-02-27 09:30:16 +0900

    first commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+export async function apiPostNoticeWrite(formData) {
+  try {
+    // console.log("API", formData);
+    return await fetch(`${API_URL}/notice/write`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((res) => res.json());
+  } catch (error) {
+    console.log(error);
+  }
+}
